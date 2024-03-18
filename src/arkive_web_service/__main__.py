f7af8a8fcca7cf188584e310c3bf23b1db41ab47
Akash <akashdotcodes@gmail.com>
2024-03-19 00:41:11 +0530

    Setup basic endpoint for google login

--- src/arkive_web_service/__main__.py
+++ src/arkive_web_service/__main__.py
@@ -1,13 +1,50 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2AuthorizationCodeBearer
 import uvicorn
+import requests
+from jose import jwt
+from dotenv import dotenv_values
+from fastapi.responses import RedirectResponse
 
-
+config = dotenv_values(".env")
 app = FastAPI()
+oauth2_scheme = OAuth2AuthorizationCodeBearer(authorizationUrl="http://localhost:8000", tokenUrl="token")
 
-@app.get("/")
-def root():
-    return "Hello, world! Welcome to Arkive!"
+GOOGLE_CLIENT_ID = config["GOOGLE_CLIENT_ID"]
+GOOGLE_CLIENT_SECRET = config["GOOGLE_CLIENT_SECRET"]
+GOOGLE_REDIRECT_URI = config["GOOGLE_REDIRECT_URI"]
+
+# TODO: add sessions here
+# TODO: move login calls to separate helpers
+# TODO: add other SSOs
+
+@app.get("/login/google", response_class=RedirectResponse)
+async def login_google():
+    return RedirectResponse(url=f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline")
+
+
+@app.get("/auth/google")
+async def auth_google(code: str):
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    data = {
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": GOOGLE_REDIRECT_URI,
+        "grant_type": "authorization_code"
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json().get("access_token", None)
+    if access_token is None:
+        return "Error!"
+    user_info = requests.get(f"https://www.googleapis.com/oauth2/v3/userinfo?access_token={access_token}") #, headers={"Authorization": f"Bearer {access_token}"})
+    return user_info.json()
+
+
+@app.get("/token")
+async def get_token(token: str = Depends(oauth2_scheme)):
+    return jwt.decode(token, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
 
 
 def main():
